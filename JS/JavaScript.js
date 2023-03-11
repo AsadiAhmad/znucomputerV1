122d69fce77124c7a0ffdfddec61e4e301920708
BlackAssassinCoder <Ahmadasadi1381.22@gmail.com>
2023-03-11 23:48:08 +0330

    add Greate JS and change table

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -1,2 +1,14 @@
-let name = 'Ahmad';
+let name = 'There is no istrage here go away!';
 console.log(name)
+
+function link_tr(){
+    var rowCount = document.getElementById('Table').rows.length;
+    for (let i = 1; i <= rowCount; i++) {
+        const row = document.getElementById(`tr${i}`);
+        row.addEventListener("click", () => {
+            window.location.href = row.dataset.href;
+        });
+    }
+}
+
+link_tr();
