a8aa5c10b7995b428245d10046c7f4061ec10b98
BlackAssassinCoder <Ahmadasadi1381.22@gmail.com>
2023-03-13 21:11:55 +0330

    fix js bug

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -3,13 +3,16 @@ let easter_egg = `There is no Easter Eggs ${egg} here go away!`;
 console.log(easter_egg)
 
 function link_tr(){
-    var rowCount = document.getElementById('Table').rows.length;
-    for (let i = 1; i <= rowCount; i++) {
-        const row = document.getElementById(`tr${i}`);
-        row.addEventListener("click", () => {
-            window.location.href = row.dataset.href;
-        });
-    }
+    var haveTable = document.getElementById('Table');
+    if (haveTable != null) {
+        var rowCount = document.getElementById('Table').rows.length;
+        for (let i = 1; i <= rowCount; i++) {
+            const row = document.getElementById(`tr${i}`);
+            row.addEventListener("click", () => {
+                window.location.href = row.dataset.href;
+            });
+        }
+    } 
 }
 
 link_tr();
