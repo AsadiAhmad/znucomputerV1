3ed32e64767c360700feae45ab3ec3b4eb51b80f
BlackAssassinCoder <Ahmadasadi1381.22@gmail.com>
2023-03-31 02:59:28 +0430

    add counter of visitors

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -21,37 +21,9 @@ function blind_user(){
     document.oncontextmenu = new Function("return false");
 }
 
-var counter = {};
-
-function manageClicks(url) {
-	if (! counter[url]) counter[url] = 0;
-	counter[url] ++;
-	alert('you clicked '+counter[url]+' '+url);
- 
-    // return location.href = url;
-	return false;
-}
-
-function init() {
-    let count = localStorage.getItem('counter');
-    if(count === null){
-        count = 0;
-        localStorage.setItem('counter', count);
-    }
-    count = parseInt(count);
-    updateCount(count);
-}
-function incrementCounter() {
-    let count = parseInt(localStorage.getItem('counter'));
-    count = count + 1;
-    localStorage.setItem('counter', count);
-    updateCount(count);
-    return true;
-}
-function updateCount(count) {
-    document.getElementById("count").innerHTML = "Clicked "+count+" times!";
+function websiteVisits(response) {
+    document.querySelector("#visits").textContent = response.value;
 }
 
 link_tr();
 blind_user();
-init();
